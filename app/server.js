1bbab25349a8a9beef1c24affb4afd137c887753
Torin <torindisalvo@gmail.com>
2017-07-31 14:30:33 -0400

    revert

--- app/server.js
+++ app/server.js
@@ -7,14 +7,24 @@ var partials = require('express-partials');
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
+var forceSSL = require('express-force-ssl');
 var helmet = require('helmet');
 
 var secure_router = require('./routes/secure_routes.js');
 var auth_router = require('./routes/auth_routes.js');
 var api_router = require('./routes/api.js');
 
-var httpsEnabled = true;
-var bypass = false;
+var httpsEnabled = false;
+var bypass = true;
+
+// SSL
+var sslkey = fs.readFileSync('../SSL/ssl-key.pem');
+var sslcert = fs.readFileSync('../SSL/ssl-cert.pem');
+
+var ssl_options = {
+    key: sslkey,
+    cert: sslcert
+};
 
 var app = express();
 var resources = ['/public', '/js'];
@@ -36,6 +46,10 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
+if (httpsEnabled) {
+	app.use(forceSSL);
+}
+
 app.use(initializeDevMode);
 app.use(helmet());
 initializeStaticRoutes()
@@ -44,7 +58,11 @@ app.use('/api', api_router)
 app.use('/auth', auth_router);
 app.use(secure_router);
 
-var secureServer = http.createServer(app);
+var port = 8080;
+var server = http.createServer(app);
+var secureServer = https.createServer(app);
+
+server.listen(8000);
 secureServer.listen(8080);
 
 function initializeStaticRoutes() {
